11f6db193d8769c4ab181949c3d15148644fa052
s7678 <s7678@abv.bg>
2023-05-08 17:33:17 +0300

    Right sided triangle

--- StarsPatterns.java
+++ StarsPatterns.java
@@ -2,19 +2,19 @@ public class StarsPatterns {
     public static void main(String[]args){
         int n=5;
 
-//        for(int i=1;i<=n;i++){
-//            for (int j=1;j<=n;j++){
-//                System.out.print("* ");
-//            }
-//            System.out.println();
-//        }
+        for(int i=1;i<=n;i++){
+            for (int j=1;j<=n;j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
         //Print Increasing Triangle
-//        for(int i=1;i<=n;i++){
-//            for (int j=1;j<=i;j++){
-//                System.out.print("* ");
-//            }
-//            System.out.println();
-//        }
+        for(int i=1;i<=n;i++){
+            for (int j=1;j<=i;j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
 
         //Print Decreasing Triangle
         for(int i=1;i<=n;i++){
@@ -23,5 +23,28 @@ public class StarsPatterns {
             }
             System.out.println();
         }
+
+        //Print Right sided Triangle I
+        for (int i=1;i<=n;i++){
+            for(int j=i;j<=n;j++){
+                System.out.print("  ");
+            }
+            for(int j=1;j<=i;j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+
+        //Print Right sided Triangle II
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=i;j++){
+                System.out.print("  ");
+            }
+            for(int j=i;j<=n;j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+
     }
 }
