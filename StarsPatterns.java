1accc99559232e1ee142e443dc3a86bcb21cb494
s7678 <s7678@abv.bg>
2023-05-08 18:05:45 +0300

    Hill Patterns

--- StarsPatterns.java
+++ StarsPatterns.java
@@ -2,43 +2,73 @@ public class StarsPatterns {
     public static void main(String[]args){
         int n=5;
 
-        for(int i=1;i<=n;i++){
-            for (int j=1;j<=n;j++){
-                System.out.print("* ");
-            }
-            System.out.println();
-        }
-        //Print Increasing Triangle
-        for(int i=1;i<=n;i++){
-            for (int j=1;j<=i;j++){
-                System.out.print("* ");
-            }
-            System.out.println();
-        }
+//        for(int i=1;i<=n;i++){
+//            for (int j=1;j<=n;j++){
+//                System.out.print("* ");
+//            }
+//            System.out.println();
+//        }
+//        //Print Increasing Triangle
+//        for(int i=1;i<=n;i++){
+//            for (int j=1;j<=i;j++){
+//                System.out.print("* ");
+//            }
+//            System.out.println();
+//        }
+//
+//        //Print Decreasing Triangle
+//        for(int i=1;i<=n;i++){
+//            for (int j=i;j<=n;j++){
+//                System.out.print("* ");
+//            }
+//            System.out.println();
+//        }
+//
+//        //Print Right sided Triangle I
+//        for (int i=1;i<=n;i++){
+//            for(int j=i;j<=n;j++){
+//                System.out.print("  ");
+//            }
+//            for(int j=1;j<=i;j++){
+//                System.out.print("* ");
+//            }
+//            System.out.println();
+//        }
+//
+//        //Print Right sided Triangle II
+//        for(int i=1;i<=n;i++){
+//            for(int j=1;j<=i;j++){
+//                System.out.print("  ");
+//            }
+//            for(int j=i;j<=n;j++){
+//                System.out.print("* ");
+//            }
+//            System.out.println();
+//        }
 
-        //Print Decreasing Triangle
-        for(int i=1;i<=n;i++){
-            for (int j=i;j<=n;j++){
-                System.out.print("* ");
-            }
-            System.out.println();
-        }
+        //Print Hill Pattern
 
-        //Print Right sided Triangle I
-        for (int i=1;i<=n;i++){
-            for(int j=i;j<=n;j++){
+//        for(int i=1;i<=n;i++){
+//            for(int j=i;j<=n;j++){
+//                System.out.print("  ");
+//            }
+//            for(int j=1;j<=i;j++){
+//                System.out.print("* ");
+//            }
+//            for(int j=1;j<i;j++){
+//                System.out.print("* ");
+//            }
+//            System.out.println();
+//        }
+
+        //Print Reverse Hill Pattern
+
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=i;j++){
                 System.out.print("  ");
             }
-            for(int j=1;j<=i;j++){
+            for(int j=i;j<n;j++){
                 System.out.print("* ");
-            }
-            System.out.println();
-        }
-
-        //Print Right sided Triangle II
-        for(int i=1;i<=n;i++){
-            for(int j=1;j<=i;j++){
-                System.out.print("  ");
             }
             for(int j=i;j<=n;j++){
                 System.out.print("* ");
