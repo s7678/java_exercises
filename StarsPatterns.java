8379144573ce25b608ee4ff29afecfe6d89d6077
s7678 <s7678@abv.bg>
2023-05-08 16:17:34 +0300

    Stars Patterns

---
+++ StarsPatterns.java
@@ -0,0 +1,27 @@
+public class StarsPatterns {
+    public static void main(String[]args){
+        int n=5;
+
+//        for(int i=1;i<=n;i++){
+//            for (int j=1;j<=n;j++){
+//                System.out.print("* ");
+//            }
+//            System.out.println();
+//        }
+        //Print Increasing Triangle
+//        for(int i=1;i<=n;i++){
+//            for (int j=1;j<=i;j++){
+//                System.out.print("* ");
+//            }
+//            System.out.println();
+//        }
+
+        //Print Decreasing Triangle
+        for(int i=1;i<=n;i++){
+            for (int j=i;j<=n;j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
