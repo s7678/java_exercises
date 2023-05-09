56c297124bb2706f8031a5e4bba99a5829a41b15
s7678 <s7678@abv.bg>
2023-05-09 21:55:51 +0300

    Hollow Square Pattern

--- StarsPatterns.java
+++ StarsPatterns.java
@@ -2,120 +2,120 @@ public class StarsPatterns {
     public static void main(String[]args){
         int n=5;
 
-//        for(int i=1;i<=n;i++){
-//            for (int j=1;j<=n;j++){
-//                System.out.print("* ");
-//            }
-//            System.out.println();
-//        }
+        for(int i=1;i<=n;i++){
+            for (int j=1;j<=n;j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
 //        //Print Increasing Triangle
-//        for(int i=1;i<=n;i++){
-//            for (int j=1;j<=i;j++){
-//                System.out.print("* ");
-//            }
-//            System.out.println();
-//        }
+        for(int i=1;i<=n;i++){
+            for (int j=1;j<=i;j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
 //
 //        //Print Decreasing Triangle
-//        for(int i=1;i<=n;i++){
-//            for (int j=i;j<=n;j++){
-//                System.out.print("* ");
-//            }
-//            System.out.println();
-//        }
+        for(int i=1;i<=n;i++){
+            for (int j=i;j<=n;j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
 //
 //        //Print Right sided Triangle I
-//        for (int i=1;i<=n;i++){
-//            for(int j=i;j<=n;j++){
-//                System.out.print("  ");
-//            }
-//            for(int j=1;j<=i;j++){
-//                System.out.print("* ");
-//            }
-//            System.out.println();
-//        }
+        for (int i=1;i<=n;i++){
+            for(int j=i;j<=n;j++){
+                System.out.print("  ");
+            }
+            for(int j=1;j<=i;j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
 //
 //        //Print Right sided Triangle II
-//        for(int i=1;i<=n;i++){
-//            for(int j=1;j<=i;j++){
-//                System.out.print("  ");
-//            }
-//            for(int j=i;j<=n;j++){
-//                System.out.print("* ");
-//            }
-//            System.out.println();
-//        }
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=i;j++){
+                System.out.print("  ");
+            }
+            for(int j=i;j<=n;j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
 
         //Print Hill Pattern
 
-//        for(int i=1;i<=n;i++){
-//            for(int j=i;j<=n;j++){
-//                System.out.print("  ");
-//            }
-//            for(int j=1;j<=i;j++){
-//                System.out.print("* ");
-//            }
-//            for(int j=1;j<i;j++){
-//                System.out.print("* ");
-//            }
-//            System.out.println();
-//        }
+        for(int i=1;i<=n;i++){
+            for(int j=i;j<=n;j++){
+                System.out.print("  ");
+            }
+            for(int j=1;j<=i;j++){
+                System.out.print("* ");
+            }
+            for(int j=1;j<i;j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
 
         //Print Reverse Hill Pattern
 
-//        for(int i=1;i<=n;i++){
-//            for(int j=1;j<=i;j++){
-//                System.out.print("  ");
-//            }
-//            for(int j=i;j<n;j++){
-//                System.out.print("* ");
-//            }
-//            for(int j=i;j<=n;j++){
-//                System.out.print("* ");
-//            }
-//            System.out.println();
-//        }
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=i;j++){
+                System.out.print("  ");
+            }
+            for(int j=i;j<n;j++){
+                System.out.print("* ");
+            }
+            for(int j=i;j<=n;j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
 
         //Print Diamond pattern
 
-//        for(int i=1;i<n;i++){
-//            for(int j=i;j<=n;j++){
-//                System.out.print("  ");
-//            }
-//            for(int j=1;j<i;j++){
-//                System.out.print("* ");
-//            }
-//            for(int j=1;j<=i;j++){
-//                System.out.print("* ");
-//            }
-//            System.out.println();
-//        }
-//        for(int i=1;i<=n;i++){
-//            for(int j=1;j<=i;j++){
-//                System.out.print("  ");
-//            }
-//            for(int j=i;j<n;j++){
-//                System.out.print("* ");
-//            }
-//            for(int j=i;j<=n;j++){
-//                System.out.print("* ");
-//            }
-//            System.out.println();
-//        }
+        for(int i=1;i<n;i++){
+            for(int j=i;j<=n;j++){
+                System.out.print("  ");
+            }
+            for(int j=1;j<i;j++){
+                System.out.print("* ");
+            }
+            for(int j=1;j<=i;j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=i;j++){
+                System.out.print("  ");
+            }
+            for(int j=i;j<n;j++){
+                System.out.print("* ");
+            }
+            for(int j=i;j<=n;j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
 
         //Print Square Parallel Bar Pattern
 
-//        for(int i=1;i<=n;i++){
-//            for(int j=1;j<=n;j++){
-//                if(j==1||j==n){
-//                    System.out.print("* ");
-//                }
-//                else{
-//                    System.out.print("  ");
-//                }
-//            }
-//            System.out.println();
-//        }
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=n;j++){
+                if(j==1||j==n){
+                    System.out.print("* ");
+                }
+                else{
+                    System.out.print("  ");
+                }
+            }
+            System.out.println();
+        }
 
         //Print Square Plus Pattern
 
@@ -136,6 +136,19 @@ public class StarsPatterns {
         for(int i=1;i<=n;i++){
             for(int j=1;j<=n;j++){
                 if(i==j||i+j==n+1){
+                    System.out.print("* ");
+                }
+                else{
+                    System.out.print("  ");
+                }
+            }
+            System.out.println();
+        }
+        //Print Hollow Square Pattern
+
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=n;j++){
+                if(i==1||j==1||i==n||j==n){
                     System.out.print("* ");
                 }
                 else{
