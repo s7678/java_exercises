55d5e1852165196143065799e4596d83ace2e8e0
s7678 <s7678@abv.bg>
2023-05-09 22:23:11 +0300

    Hollow Decreasing Triangle

--- StarsPatterns.java
+++ StarsPatterns.java
@@ -172,6 +172,20 @@ public class StarsPatterns {
             System.out.println();
         }
 
+        //Print Hollow Decreasing Triangle
+
+        for(int i=1;i<=n;i++){
+            for(int j=i;j<=n;j++){
+                if(i==1||j==i||j==n){
+                    System.out.print("* ");
+                }
+                else{
+                    System.out.print("  ");
+                }
+            }
+            System.out.println();
+        }
+
 
 
 
