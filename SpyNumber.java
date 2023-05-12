06e8b58a2aba9c7b781e4be0f646ba3480fdc97b
s7678 <s7678@abv.bg>
2023-05-12 22:33:50 +0300

    SpyNumber

---
+++ SpyNumber.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class SpyNumber {
+    public static void main(String[]args){
+        Scanner scanner=new Scanner(System.in);
+        System.out.println("Enter a number");
+        int number= scanner.nextInt();
+        int num=number;
+
+        int sumOfDigits=0;
+        int productOfDigits=1;
+        int temp;
+        while(num>0){
+            temp=num%10;
+            sumOfDigits=sumOfDigits+temp;
+            productOfDigits=productOfDigits*temp;
+            num=num/10;
+        }
+
+        if(sumOfDigits==productOfDigits){
+            System.out.println(number+" its a spy number");
+        }
+        else{
+            System.out.println(number+" its NOT a spy number");
+        }
+
+
+
+
+    }
+
+}
